40a581045d2526c7d7310f9cc39809f266d8b6b6
vanessaMarcuse <babacar.diakte01@gmail.com>
2023-01-12 21:19:16 -0500

    Add userDropDown

--- CandySoap/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ CandySoap/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -17,7 +17,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 
@@ -112,8 +114,10 @@ namespace CandySoap.Areas.Identity.Pages.Account
 			public string? City { get; set; }
 			public string? State { get; set; }
 			public string? PostalCode { get; set; }
-            public string PhoneNumber { get; set; }
-
+            public string? PhoneNumber { get; set; }
+            public string? Role { get; set; }   
+            [ValidateNever]
+           public IEnumerable<SelectListItem> RoleList { get; set; }  
 
 
 		}
@@ -130,6 +134,14 @@ namespace CandySoap.Areas.Identity.Pages.Account
             }
 			ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            Input = new InputModel()
+            {
+                RoleList = _roleManager.Roles.Select(x => x.Name).Select(i=>new SelectListItem
+                {
+                    Text = i,
+                    Value = i
+                })
+            };
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
